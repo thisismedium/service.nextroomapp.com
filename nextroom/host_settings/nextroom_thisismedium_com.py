7d549cc54730de8a2019c89015cf203990886f60
coptix <bob@herod.hosts.coptix.com>
2010-05-12 14:52:41 -0400

    new server config

--- nextroom/host_settings/nextroom_thisismedium_com.py
+++ nextroom/host_settings/nextroom_thisismedium_com.py
@@ -1,7 +1,7 @@
 from common import *
 DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'nextroom_jdev'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'nextroom_jdev'             # Not used with sqlite3.
+DATABASE_NAME = 'nextroom_thisismedium_com'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'nextroom_thisismedium_com'             # Not used with sqlite3.
 DATABASE_PASSWORD = SECRET_KEY         # Not used with sqlite3.
 DATABASE_HOST = 'psqlhost'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
