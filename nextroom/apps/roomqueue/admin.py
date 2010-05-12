2d30ca5c1b73ae769e7e5dff13330e0471e18805
Bob Waycott <bob@bob.local>
2010-05-12 18:33:18 -0400

    update urls

--- nextroom/apps/roomqueue/admin.py
+++ nextroom/apps/roomqueue/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from nextroom.apps.roomqueue.models import Version, Note, Procedure, User, Room
 from nextroom.apps.roomqueue.customfields import ColorPickerWidget, ColorField
 
-admin.site.register(Version)
+#admin.site.register(Version)
 admin.site.register(Note)
 admin.site.register(Procedure)
 
