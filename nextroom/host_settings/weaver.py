19e9d182a7abf6b700c76461203743a4e8ae2995
Ben Weaver <ben@orangesoda.net>
2010-09-21 15:25:12 -0400

    Add weaver settings.

---
+++ nextroom/host_settings/weaver.py
@@ -0,0 +1,9 @@
+from common import *
+
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'nextroom'
+DATABASE_USER = 'nextroom'
+DATABASE_PASSWORD = 'nextroom123'
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = ''
+DEBUG = True
