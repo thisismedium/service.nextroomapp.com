466c028e4c1cb3b2f18c8860a04be46fd4943986
Bob Waycott <bob@bob.local>
2010-09-28 18:31:41 -0400

    updated my settings to work in MVM without affecting Weaver's non-MVM setup

--- nextroom/host_settings/bob.py
+++ nextroom/host_settings/bob.py
@@ -1,8 +1,21 @@
 from common import *
+from os.path import *
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
 DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'nextroom'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'nextroom'             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = 'parent'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-DEBUG = True
+
+ROOT_URLCONF = 'nextroom.urls'
+
+TEMPLATE_DIRS = (
+    join(PROJECT_DIR, 'templates')
+)
+
+MEDIA_URL = '/media/'
