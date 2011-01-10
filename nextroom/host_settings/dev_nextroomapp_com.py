8406f76fe42193204dbb3c91ef43762f0f1559fc
coptix <Ben Weaver weaver@coptix.com>
2011-01-10 11:47:15 -0500

    Updated dev site config and runserver

---
+++ nextroom/host_settings/dev_nextroomapp_com.py
@@ -0,0 +1,9 @@
+from common import *
+
+DEBUG = True
+
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'dev_nextroomapp_com'
+DATABASE_USER = 'dev_nextroomapp_com'
+DATABASE_PASSWORD = SECRET_KEY
+DATABASE_HOST = 'psqlhost'
