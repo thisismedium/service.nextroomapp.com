664c417d3690a0e1a78e80698979c35107e94e22
weaver@coptix.com <weaver@coptix.com>
2011-01-17 13:33:30 -0500

    Live settings file.

---
+++ nextroom/host_settings/service_nextroomapp_com.py
@@ -0,0 +1,7 @@
+from common import *
+
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'service_nextroomapp_com'
+DATABASE_USER = 'service_nextroomapp_com'
+DATABASE_PASSWORD = SECRET_KEY
+DATABASE_HOST = 'psqlhost'
